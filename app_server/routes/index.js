f034312445af4c7d24123964173677d79cd5669b
Guilherme Rodriguês <guiihpr@gmail.com>
2016-10-29 01:01:26 -0200

    Página 134

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations'),
+    ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index );
+
+/* Locations pages */
+router.get('/', ctrlLocations.homeList);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+/* Others Page */
+router.get('/about', ctrlOthers.about);
+
+
+
 
 module.exports = router;
